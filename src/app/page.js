8067e1b3576c2d39ab1f2667f65c91cc26de1070
Aurelienjunior <tsangueboma@gmail.com>
2025-04-16 00:05:45 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className=" flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="  ">
+    <div className=" w-full h-fit flex flex-col items-center justify-center p-4">
+      {/* <div className="  ">
         <h1 className="text-3xl font-bold">Welcome to Aurelien Dev</h1>
         <p className="mt-4 text-lg">
           This is my portfolio website, showcasing my projects and skills. In
@@ -16,7 +16,10 @@ export default function Home() {
           applications. I enjoy working with modern technologies and frameworks
           to build user-friendly interfaces and robust backends.
         </p>
-      </div>
+      </div> */}
+
+      {/* Landing Secton */}
+      <div className=" relative w-full min-h-screen bg-[#0269F3] flex flex-col items-center justify-center  "></div>
     </div>
   );
 }
